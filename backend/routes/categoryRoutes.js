87aa3928e8451b53bab3f31a3fc91972d8538ce6
Nicolas Silva Pereira <njsilvapereira@gmail.com>
2023-10-20 14:24:38 -0300

    delete category

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -3,5 +3,7 @@ const route = express.Router();
 const categoryController = require("../controllers/categoryController");
 
 route.post("/crear/category", categoryController.createCategory);
+route.get("/categories", categoryController.getCategory);
+route.delete("/delete/category/:id", categoryController.deleteCategory);
 
 module.exports = route;
